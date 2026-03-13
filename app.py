ba0654b030d2e6c17d3024179c93e9baf50b6dd0
Nadav Cornblum <corn.nad@gmail.com>
2026-03-13 14:16:19 +0200

    fixed port

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ def filters():
 
 
 if __name__ == "__main__":
-    app.run(debug=True) #need to add 0.0.0.0 for free access
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
